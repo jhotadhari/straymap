a54baa3967548de7231025c86ca229ba12daeda1
jhotadhari <tellme@waterproof-webdesign.de>
2024-10-22 18:05:24 +0200

    Setup multilang

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,9 +16,15 @@ import {
 	useWindowDimensions,
 	PixelRatio,
 	View,
+	Pressable,
 	NativeEventEmitter,
 } from 'react-native';
 
+
+import '../assets/i18n/i18n';
+
+import 'intl-pluralrules';
+import {useTranslation} from 'react-i18next';
 
 const App = () => {
 
@@ -28,6 +34,22 @@ const App = () => {
 		backgroundColor: isDarkMode ? 'black' : '#eee',
 		color: isDarkMode ? '#eee' : 'black',
 	};
+
+
+
+
+	const {t, i18n} = useTranslation();
+
+	const [currentLanguage,setLanguage] = useState( 'en' );
+
+	const changeLanguage = value => {
+		i18n
+			.changeLanguage( value )
+			.then( () => setLanguage( value ))
+			.catch( err => console.log( err ) );
+	};
+
+
 
 	const {
 		width,
@@ -56,8 +78,36 @@ const App = () => {
 
 				<Text
 					style={ { marginBottom: 10 } }
-				>That's just a test string</Text>
+				>{ t( 'test' ) }</Text>
 
+				<View
+					style={ {
+						width,
+						justifyContent: 'space-evenly',
+						alignItems: 'center',
+						flexDirection: 'row',
+					} }
+				>
+					<Pressable
+						onPress={ () => changeLanguage( 'en' ) }
+						style={ {
+							backgroundColor: currentLanguage === 'en' ? '#555' : style.backgroundColor,
+							padding: 10,
+						} }
+					>
+						<Text>Select English</Text>
+					</Pressable>
+
+					<Pressable
+						onPress={ () => changeLanguage( 'de' ) }
+						style={{
+							backgroundColor: currentLanguage === 'de' ? '#555' : style.backgroundColor,
+						} }
+					>
+						<Text>Sprache wählen</Text>
+					</Pressable>
+
+				</View>
 			</View>
 
 		</SafeAreaView>
