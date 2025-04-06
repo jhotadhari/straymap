dac1c1a91cb7938e3442fb3ddea3fe27c89b4a7e
jhotadhari <tellme@waterproof-webdesign.de>
2025-04-07 01:05:43 +0200

    Move hook useSettings

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -22,7 +22,6 @@ import {
 	PaperProvider,
 	useTheme,
 } from 'react-native-paper';
-import useDeepCompareEffect from 'use-deep-compare-effect'
 import { get, pick, without } from 'lodash-es';
 import { sprintf } from 'sprintf-js';
 import semverCompare from 'semver-compare';
@@ -65,6 +64,7 @@ import { defaults } from '../constants';
 import SplashScreen from './SplashScreen';
 import AppView from './AppView';
 import SplashScreenUpdater from './SplashScreenUpdater';
+import useSettings from '../compose/useSettings';
 
 const useAppTheme = () => {
 
@@ -214,71 +214,6 @@ const AppWrapper = () => {
 			changeLang={ changeLang }
 		/>
 	</PaperProvider>;
-};
-
-const mergeSettingsForKey = ( initialSettings: object, newSettings: object, key: string ) => ( {
-	[key]: {
-		...get( initialSettings, key, {} ),
-		...( newSettings.hasOwnProperty( key ) && pick(
-			get( newSettings, key, {} ),
-			Object.keys( get( initialSettings, key, {} ) )
-		) ),
-	},
-} );
-
-const useSettings = ( {
-	maybeIsBusyAdd,
-	maybeIsBusyRemove,
-	settingsKey,
-	savedMessage,
-	initialSettings = {},
-} : {
-	maybeIsBusyAdd?: ( key: string ) => void;
-	maybeIsBusyRemove?: ( key: string ) => void;
-	savedMessage?: string;
-	settingsKey: string;
-	initialSettings?: object;
-} ) => {
-
-	const [initialized,setInitialized] = useState( false );
-	const [settings,setSettings] = useState<object>( initialSettings );
-
-    useEffect( () => {
-		const busyKey = 'useSettings' + 'load' + settingsKey;
-		maybeIsBusyAdd && maybeIsBusyAdd( busyKey );
-		setTimeout( () => DefaultPreference.get( settingsKey ).then( newSettingsStr => {
-			if ( newSettingsStr ) {
-				const newSettings = JSON.parse( newSettingsStr );
-				setSettings( {
-					...initialSettings,
-					...newSettings,
-					...( 'generalSettings' === settingsKey && {
-						...mergeSettingsForKey( initialSettings, newSettings, 'unitPrefs' ),
-						...mergeSettingsForKey( initialSettings, newSettings, 'dashboardElements' ),
-					} ),
-				} );
-			}
-			setInitialized( true );
-		} ).catch( err => 'ERROR' + console.log( err ) )
-		.finally( () => maybeIsBusyRemove && maybeIsBusyRemove( busyKey ) ), 1 );
-    }, [] );
-
-	useDeepCompareEffect( () => {
-		if ( initialized ) {
-			const busyKey = 'useSettings' + 'changed' + settingsKey;
-			maybeIsBusyAdd && maybeIsBusyAdd( busyKey );
-			setTimeout( () => DefaultPreference.set( settingsKey, JSON.stringify( settings ) )
-			.then( () => savedMessage ? ToastAndroid.show( savedMessage, ToastAndroid.SHORT ) : null )
-			.catch( err => 'ERROR' + console.log( err ) )
-			.finally( () => maybeIsBusyRemove && maybeIsBusyRemove( busyKey ) ), 10 );
-		}
-	}, [settings] )
-	return {
-		settings,
-		setSettings,
-		initialized,
-	};
-
 };
 
 const useInitialCenter = ( currentMapEvent: MapEventResponse ) => {
---
+++ src/compose/useSettings.ts
@@ -0,0 +1,81 @@
+/**
+ * External dependencies
+ */
+import { get, pick } from 'lodash-es';
+import {
+	useEffect,
+	useState,
+} from 'react';
+import {
+	ToastAndroid,
+} from 'react-native';
+import DefaultPreference from 'react-native-default-preference';
+import useDeepCompareEffect from 'use-deep-compare-effect'
+
+const mergeSettingsForKey = ( initialSettings: object, newSettings: object, key: string ) => ( {
+    [key]: {
+        ...get( initialSettings, key, {} ),
+        ...( newSettings.hasOwnProperty( key ) && pick(
+            get( newSettings, key, {} ),
+            Object.keys( get( initialSettings, key, {} ) )
+        ) ),
+    },
+} );
+
+const useSettings = ( {
+	maybeIsBusyAdd,
+	maybeIsBusyRemove,
+	settingsKey,
+	savedMessage,
+	initialSettings = {},
+} : {
+	maybeIsBusyAdd?: ( key: string ) => void;
+	maybeIsBusyRemove?: ( key: string ) => void;
+	savedMessage?: string;
+	settingsKey: string;
+	initialSettings?: object;
+} ) => {
+
+	const [initialized,setInitialized] = useState( false );
+	const [settings,setSettings] = useState<object>( initialSettings );
+
+    useEffect( () => {
+		const busyKey = 'useSettings' + 'load' + settingsKey;
+		maybeIsBusyAdd && maybeIsBusyAdd( busyKey );
+		setTimeout( () => DefaultPreference.get( settingsKey ).then( newSettingsStr => {
+			if ( newSettingsStr ) {
+				const newSettings = JSON.parse( newSettingsStr );
+				setSettings( {
+					...initialSettings,
+					...newSettings,
+					...( 'generalSettings' === settingsKey && {
+						...mergeSettingsForKey( initialSettings, newSettings, 'unitPrefs' ),
+						...mergeSettingsForKey( initialSettings, newSettings, 'dashboardElements' ),
+					} ),
+				} );
+			}
+			setInitialized( true );
+		} ).catch( err => 'ERROR' + console.log( err ) )
+		.finally( () => maybeIsBusyRemove && maybeIsBusyRemove( busyKey ) ), 1 );
+    }, [] );
+
+	useDeepCompareEffect( () => {
+		if ( initialized ) {
+			const busyKey = 'useSettings' + 'changed' + settingsKey;
+			maybeIsBusyAdd && maybeIsBusyAdd( busyKey );
+			setTimeout( () => DefaultPreference.set( settingsKey, JSON.stringify( settings ) )
+			.then( () => savedMessage ? ToastAndroid.show( savedMessage, ToastAndroid.SHORT ) : null )
+			.catch( err => 'ERROR' + console.log( err ) )
+			.finally( () => maybeIsBusyRemove && maybeIsBusyRemove( busyKey ) ), 10 );
+		}
+	}, [settings] );
+
+	return {
+		settings,
+		setSettings,
+		initialized,
+	};
+
+};
+
+export default useSettings;
