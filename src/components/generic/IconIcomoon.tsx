2c7b5885754bb977a37b745cb945645492aef8e6
jhotadhari <tellme@waterproof-webdesign.de>
2026-06-09 14:59:31 +0200

    Upgrade react-native-vector-icons manually

--- src/components/generic/IconIcomoon.tsx
+++ src/components/generic/IconIcomoon.tsx
@@ -1,13 +1,23 @@
 /**
  * External dependencies
  */
-import { createIconSetFromIcoMoon } from 'react-native-vector-icons';
+// import { createIconSetFromIcoMoon } from 'react-native-vector-icons';
+import { createIconSet } from '@react-native-vector-icons/common';
 
 /**
  * Internal dependencies
  */
 import icoMoonConfig from '../../assets/icons/icomoon/selection.json';
 
-const IconIcomoon = createIconSetFromIcoMoon(icoMoonConfig, 'icomoon');
+const glyphMap = icoMoonConfig.icons.reduce<{ [key: string]: number }>(( acc, icon) => {
+	acc[icon.properties.name] = icon.properties.code;
+    return acc;
+}, {} );
+
+const IconIcomoon = createIconSet(glyphMap, {
+	postScriptName: 'icomoon',
+	fontFileName: 'icomoon.ttf',
+	fontSource: require('../../assets/icons/icomoon/fonts/icomoon.ttf'), // optional, for dynamic loading. Can also be a local file uri.
+});
 
 export default IconIcomoon;
